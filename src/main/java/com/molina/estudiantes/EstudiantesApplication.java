8c12f805800dbbe7078980fcca2420341065a79d
Kenny Molina <kjmolina@live.com>
2023-09-14 09:47:26 -0600

    first commit

---
+++ src/main/java/com/molina/estudiantes/EstudiantesApplication.java
@@ -0,0 +1,11 @@
+package com.molina.estudiantes;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudiantesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EstudiantesApplication.class, args);
+    }
+}
+
---
+++ src/test/java/com/molina/estudiantes/EstudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.molina.estudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
